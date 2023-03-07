d10fd21db1a2c969a117e23964b037249cfd5a20
zimasakasa <127197228+zimasakasa@users.noreply.github.com>
2023-03-07 04:12:33 -0800

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,8 @@
+function showAlert() {
+  //create variable 'myText' with string value
+  let myText = "Hello World";
+  // passing myText variable to alert method
+  alert (myText);
+  // write to browser inspector console
+  console.log('the "myText" string is: ' + myText + '!');
+}
